0c97dce87a5bfb2b92912f4d9f582b37a0320b41
Antosh <antosh.dyade@gmail.com>
2024-08-13 02:21:36 +0530

    Update CalculatorServlet.java

--- src/main/java/com/example/calculator/CalculatorServlet.java
+++ src/main/java/com/example/calculator/CalculatorServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/CalculatorServlet")
+@WebServlet("/calculate")
 public class CalculatorServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
